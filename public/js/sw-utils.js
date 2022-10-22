2290dc906e178f6bd51ee56c0ef2eeed16bba57d
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-22 11:36:54 -0500

    106. Envío de la petición POST

--- public/js/sw-utils.js
+++ public/js/sw-utils.js
@@ -43,14 +43,25 @@ function actualizaCacheStatico( staticCache, req, APP_SHELL_INMUTABLE ) {
 // Network with cache fallback / update
 function manejoApiMensajes( cacheName, req ) {
     
-    return fetch( req ).then(res => {
-        if (res.ok) {
-            actualizaCacheDinamico( cacheName, req, res.clone() );
-            return res.clone();
-        } else {
+    if ( req.clone().method === 'POST' ) {
+        //POSTEO de un nuevo mensaje
+
+        // tengo que guardar en el indexDB
+
+        return fetch( req );
+
+    } else {
+
+        return fetch( req ).then(res => {
+            if (res.ok) {
+                actualizaCacheDinamico( cacheName, req, res.clone() );
+                return res.clone();
+            } else {
+                return caches.match( req );
+            }
+        }).catch( err => {
             return caches.match( req );
-        }
-    }).catch( err => {
-        return caches.match( req );
-    });
+        });
+    }
+
 }
--- server/routes.js
+++ server/routes.js
@@ -28,6 +28,8 @@ router.post('/', function (req, res) {
   }
   mensajes.push( mensaje );
 
+  console.log(mensajes);
+
   res.json({
     ok: true,
     mensaje
