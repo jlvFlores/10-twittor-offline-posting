1728fbfa5066aa427ed78d8e90d90bc0d57beb94
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-22 10:38:17 -0500

    102. API REST - Get Mensajes

--- server/routes.js
+++ server/routes.js
@@ -3,14 +3,19 @@ var express = require('express');
 var router = express.Router();
 
 
-
+const mensajes = {
+  id: 'XXX',
+  user: 'Spiderman',
+  mensaje: 'Hola mundo'
+}
 
 
 
 
 // Get mensajes
 router.get('/', function (req, res) {
-  res.json('Obteniendo mensajes');
+  // res.json('Obteniendo mensajes');
+  res.json( mensajes );
 });
 
 
